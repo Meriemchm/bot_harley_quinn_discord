45b676cc8d4ca6f7f09b53362d9ecbb860bc90ee
Meriemchm <chamimeriem2302@gmail.com>
2023-09-19 17:26:31 +0100

    v1

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = "MTE1MzY3MTA2Njk4ODg1OTQwMw.GWokSr.YDJjRBe6jcUjUzy-Qdut4HGwbnvBOXy7yNAWoE"
+    TOKEN = "its private i cant show it"
     intents = discord.Intents.default()
     intents.message_content = True
 
